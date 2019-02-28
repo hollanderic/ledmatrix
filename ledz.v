be1504eac68f19c6dbefe53440287809466cb2de
Eric Holland <hollande@google.com>
2019-02-28 13:08:02 -0800

    16bit color

--- ledz.v
+++ ledz.v
@@ -4,11 +4,12 @@
 
 `define ROWLEN 64
 
-module mem(input clk, wen, input [7:0] addr, input [15:0] wdata, output reg [15:0] rdata);
-  reg [15:0] mem [0:255];
+module mem(input clk, wen, input [9:0] addr, input [15:0] wdata, output reg [15:0] rdata);
+  reg [15:0] mem [0:1023];
   initial mem[0] = 255;
   initial mem[22] = 1;
   initial mem[7] = 1;
+  initial mem[1000] = 2;
   always @(posedge clk) begin
         if (wen) mem[addr] <= wdata;
         rdata <= mem[addr];
@@ -38,30 +39,12 @@ module top (
     reg [63:0] pix [0:15];
     reg [3:0] rowsel;
 
-    initial pix[0] = 8'h01;
-/*
-    initial pix[1] = 64'h0000000000000001;
-    initial pix[2] = 64'h8000000000000000;
-    initial pix[3] = 64'h0000000000000000;
-    initial pix[4] = 64'h0000000000000000;
-    initial pix[5] = 64'h0000000000000000;
-    initial pix[6] = 64'h0000000000000000;
-    initial pix[7] = 64'h0000000000000000;
-    initial pix[8] = 64'h0000000000000000;
-    initial pix[9] = 64'h0000000000000000;
-    initial pix[10] = 64'h0000000000000000;
-    initial pix[11] = 64'h0000000000000000;
-    initial pix[12] = 64'h0000000000000000;
-    initial pix[13] = 64'h0000000000000000;
-    initial pix[14] = 64'h0000000000000000;
-    initial pix[15] = 64'h0000000000000000;
-    initial pix[15] = 64'hffffffffffffffff;
-*/
+    reg clk2;
 
     initial rowsel = 4'hf;
     initial stb = 0;
 
-
+    reg[4:0] scan_counter;
     reg[7:0] counter;
 
     initial counter = 7'h0;
@@ -72,30 +55,38 @@ module top (
     assign sel_d = rowsel[3];
 
     wire [15:0] dbus;
+    wire [10:0] abus;
 
-    mem mem_0(clk, 0, counter[5:0], 16'h0000, dbus);
+    assign abus = {rowsel, counter[5:0]};
 
+    mem mem_0(clk, 0, abus, 16'h0000, dbus);
+
+    always @(rowsel) begin
+        if (rowsel == 0)
+         scan_counter <= scan_counter + 1;
+    end
 
     always @(posedge clk) begin
-        if (stb) begin
-         counter <= 0;
-         rowsel <= rowsel + 1;
-         pix[0] <= pix[0] + 1;
-        end else
-         counter <= counter +1;
+        clk2 <= clk2 + 1;
     end
 
 
-
-
     always @(negedge clk) begin
-        if (counter == 64) begin
-            stb <= 1;
-            oe <= 1;
-        end else begin
-            stb <= 0;
-            oe <= 0;
-        end
+        if (clk2 == 0) begin
+            if (counter == 64) begin
+                stb <= 1;
+                oe <= 1;
+            end else begin
+                stb <= 0;
+                oe <= 0;
+            end
+            if (counter > 64) begin
+                rowsel <= rowsel + 1;
+                counter <= 0;
+            end
+        end else
+            if (counter == 64)
+                oe <= 1;
     end
 
 
@@ -103,22 +94,25 @@ module top (
 // This is where we need to update out data values
 
     always @ (negedge clk) begin
-        if (counter < 64) begin
+        if (clk2)
+            if (counter < 64) begin
 
-            if (dbus[0])
-                redout0 <= 1;
-            else
-                redout0 <= 0;
+                if (dbus[15:11]  > scan_counter)
+                    redout0 <= 1;
+                else
+                    redout0 <= 0;
 
-//            if (pix[rowsel][counter])
-//                greenout0 <= 1;
-//            else
-                greenout0 <= 0;
-//            if (pix[rowsel][counter])
-//                blueout0 <= 1;
-//            else
-                blueout0 <= 0;
-        end
+                if (dbus[10:5]  > scan_counter)
+                    greenout0 <= 1;
+                else
+                    greenout0 <= 0;
+                if (dbus[4:0]  > scan_counter)
+                    blueout0 <= 1;
+                else
+                    blueout0 <= 0;
+
+
+            end
     end
 
 
@@ -126,8 +120,8 @@ module top (
 // This is where we update the clock pin
 //  don't make a positive edge during the latch period
 
-    always @ (clk) begin
-        if (clk) begin
+    always @ (clk2) begin
+        if (clk2) begin
           if (~stb)
             clkout <= 1;
         end else
